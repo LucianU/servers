cf259570f13ce6a649dee7ac33abf20ca58ef1fc
Lucian Ursu <lucian.ursu@gmail.com>
2022-08-13 09:13:39 +0300

    Restructure home.nix

--- macbook/home.nix
+++ macbook/home.nix
@@ -1,87 +1,85 @@
 { config, pkgs, lib, ... }:
 
 {
-  home.stateVersion = "22.05";
+  home = {
+    stateVersion = "22.05";
 
-  home.username = "lucian";
-  home.homeDirectory = "/Users/lucian";
+    username = "lucian";
+    homeDirectory = "/Users/lucian";
 
-  home.packages = with pkgs; [
-    coreutils
-    curl
-    wget
-    jq
-    cachix
-    git
-    ripgrep
-    vagrant
-  ];
-  home.sessionVariables = {
-    EDITOR = "nvim";    
-    VISUAL = "nvim";
-    HOMEBREW_PREFIX = "/opt/homebrew";
-    HOMEBREW_CELLAR = "/opt/homebrew/Cellar";
-    HOMEBREW_REPOSITORY = "/opt/homebrew";
+    packages = with pkgs; [
+      coreutils
+      curl
+      wget
+      jq
+      cachix
+      git
+      ripgrep
+      vagrant
+    ];
+
+    sessionVariables = {
+      EDITOR = "nvim";    
+      VISUAL = "nvim";
+      HOMEBREW_PREFIX = "/opt/homebrew";
+      HOMEBREW_CELLAR = "/opt/homebrew/Cellar";
+      HOMEBREW_REPOSITORY = "/opt/homebrew";
+    };
+    sessionPath = [
+      "/nix/var/nix/profiles/default/bin"
+      "$HOME/.nix-profile"
+      "/opt/homebrew/bin"
+    ];
   };
-  home.sessionPath = [
-    "/nix/var/nix/profiles/default/bin"
-    "$HOME/.nix-profile"
-    "/opt/homebrew/bin"
-  ];
 
   nix.enable = true;
 
-  programs.home-manager = {
-    enable = true;
-    path = "${config.home.homeDirectory}/.config/nixpkgs/home.nix";
-  };
-
-  programs.zsh = {
-    enable = true;
-    shellAliases = {
-      ll = "ls -al";
-    };
-  };
-
-  programs.direnv = {
-    enable = true;
-    nix-direnv.enable = true;
-  };
-
-  programs.htop = {
-    enable = true;
-    settings.show_program_path = true;
-  };
-
-  programs.git = {
-    enable = true;
-    userName = "Lucian Ursu";
-    userEmail = "lucian.ursu@gmail.com";
-    delta = {
+  programs = {
+    home-manager = {
       enable = true;
-      options = {
-        side-by-side = true;
-        line-numbers = true;
+      path = "${config.home.homeDirectory}/.config/nixpkgs/home.nix";
+    };
+    zsh = {
+      enable = true;
+      shellAliases = {
+        ll = "ls -al";
       };
     };
-  };
-
-  programs.neovim = {
-    enable = true;
-    viAlias = true;
-    vimAlias = true;
-    plugins = with pkgs.vimPlugins; [
-      vim-nix
-      direnv-vim
-    ];
-    extraConfig = builtins.readFile ./neovim-settings.lua;
-  };
-
-  programs.starship = {
-    enable = true;
-  };
-
-  programs.irssi = {
-    enable = true;
+    direnv = {
+      enable = true;
+      nix-direnv.enable = true;
+    };
+    htop = {
+      enable = true;
+      settings.show_program_path = true;
+    };
+    git = {
+      enable = true;
+      userName = "Lucian Ursu";
+      userEmail = "lucian.ursu@gmail.com";
+      delta = {
+        enable = true;
+        options = {
+          side-by-side = true;
+          line-numbers = true;
+        };
+      };
+    };
+    neovim = {
+      enable = true;
+      viAlias = true;
+      vimAlias = true;
+      plugins = with pkgs.vimPlugins; [
+        vim-nix
+        direnv-vim
+      ];
+      extraConfig = builtins.readFile ./neovim-settings.lua;
+    };
+    starship = {
+      enable = true;
+    };
+    irssi = {
+      enable = true;
+    };
   };
 }

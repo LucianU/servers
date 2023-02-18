14ff8669d1da72c5103ebb15a98e8d8e2bef19de
Lucian Ursu <lucian.ursu@gmail.com>
2023-02-18 13:35:50 +0200

    Add nixbuild.net as remote builder

--- macbook/configuration.nix
+++ macbook/configuration.nix
@@ -10,6 +10,14 @@
   nix.trustedUsers = [
     "@admin"
   ];
+  nix.distributedBuilds = true;
+  nix.buildMachines = [
+    { hostName = "eu.nixbuild.net";
+      system = "x86_64-linux";
+      maxJobs = 100;
+      supportedFeatures = [ "benchmark" "big-parallel" ];
+    }
+  ];
 
   users.nix.configureBuildUsers = true;
   users.users.lucian = {

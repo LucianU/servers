fca4d8a56baf28329006926b273f4f0fdd488105
Lucian Ursu <lucian.ursu@gmail.com>
2023-04-03 18:15:30 +0300

    Add hetzner-main as build machine
    Also remove nixbuild.net.

--- macbook/configuration.nix
+++ macbook/configuration.nix
@@ -12,7 +12,7 @@
   ];
   nix.distributedBuilds = true;
   nix.buildMachines = [
-    { hostName = "eu.nixbuild.net";
+    { hostName = "hetzner-main";
       system = "x86_64-linux";
       maxJobs = 100;
       supportedFeatures = [ "benchmark" "big-parallel" ];

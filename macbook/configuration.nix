a9c1ad2c107d3ab2d194ecab866a627fd7a19a9f
Lucian Ursu <lucian.ursu@gmail.com>
2023-04-04 18:37:40 +0300

    Add nix option builders-use-substitutes

--- macbook/configuration.nix
+++ macbook/configuration.nix
@@ -27,6 +27,7 @@
 
   nix.extraOptions = ''
     auto-optimise-store = true
+    builders-use-substitutes = true
     experimental-features = nix-command flakes
     extra-platforms = x86_64-darwin aarch64-darwin
   '';

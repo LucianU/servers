caaef77c43fa460b801426eff2bddb37b722bcd2
Lucian Ursu <lucian.ursu@gmail.com>
2023-04-20 16:15:22 +0300

    Remove age-keys from flake
    I changed the sops-nix configuration so it uses the
    SSH Host Key of the target machine.

--- flake.nix
+++ flake.nix
@@ -5,8 +5,6 @@
     sops-nix.url = "github:Mic92/sops-nix";
     sops-nix.inputs.nixpkgs.follows = "nixpkgs";
 
-    age-keys.url = "/Users/lucian/.config/sops/age";
-
     knowledge-store.url = "/Users/lucian/code/web/knowledge-store";
     knowledge-store.inputs.nixpkgs.follows = "nixpkgs";
 
@@ -14,7 +12,7 @@
     wikis.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = inputs@{ nixpkgs, sops-nix, age-keys, ... }: {
+  outputs = inputs@{ nixpkgs, sops-nix, ... }: {
     nixosConfigurations = {
       "knowledge-store" = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";

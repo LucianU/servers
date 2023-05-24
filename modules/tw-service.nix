47e560d4ae9c671917cf4d21178fe208416a7ef6
Lucian Ursu <lucian.ursu@gmail.com>
2023-05-24 16:10:18 +0300

    Finalize tw-service

--- modules/tw-service.nix
+++ modules/tw-service.nix
@@ -1,13 +1,14 @@
-{ service-name, pkg, config, lib, pkgs, ... }:
-  with lib;
+{ service-name, service-pkg, config, lib, pkgs, ... }:
 
-	let
-    cfg = config.services.${service-name};
-    service-pkg = pkg;
-    description = "${service-name} - TiddlyWiki Instance";
-  in
-  {
-    options = {
+with lib;
+
+let
+  description = "${service-name} - Tiddlywiki instance";
+  cfg = config.services.${service-name};
+in
+  let
+    options =  {
+
       enable = mkEnableOption description;
 
       package = mkOption {
@@ -17,7 +18,7 @@
       };
 
       port = mkOption {
-        type = types.int;
+        type = types.port;
         default = null;
         description = "The port on which the TiddlyWiki instance will listen.";
       };
@@ -34,29 +35,56 @@
         description = "The domain name of the TiddlyWiki instance.";
       };
 
-      backupRepo = mkOption {
-        type = types.path;
-        default = null;
-        description = "The URL of the Cloud Object Storage bucket where backups will be stored.";
+      backup = {
+        backend = {
+          url = mkOption {
+            type = types.str;
+            default = null;
+            description = "The URL of the Cloud Object Storage bucket where backups will be stored.";
+          };
+
+          credentialsFile = mkOption {
+            type = types.path;
+            default = null;
+            description = "The file that contains the credentials used to access the Cloud Object Storage bucket.";
+          };
+        };
+
+        passwordFile = mkOption {
+          type = types.path;
+          default = null;
+          description = "The file that contains the password used to encrypt backups.";
+        };
       };
 
-      backupPasswordFile = mkOption {
-        type = types.path;
-        default = null;
-        description = "The file that contains the password used to encrypt backups.";
+      restore = {
+        backend = {
+          url = mkOption {
+            type = types.str;
+            default = cfg.backup.backend.url;
+            description = "The URL of the Cloud Object Storage bucket where backups are stored.";
+          };
+
+          credentialsFile = mkOption {
+            type = types.path;
+            default = cfg.backup.backend.credentialsFile;
+            description = "The file that contains the credentials used to access the Cloud Object Storage bucket.";
+          };
+        };
+
+        passwordFile = mkOption {
+          type = types.path;
+          default = cfg.backup.passwordFile;
+          description = "The file that contains the password used to encrypt backups.";
+        };
       };
 
-      backupCloudCredentialsFile = mkOption {
-        type = types.path;
-        default = null;
-        description = "The file that contains the credentials used to access the Cloud Object Storage bucket.";
-      };
 
       listenOptions = mkOption {
         type = types.attrs;
 
         default = {
-          credentials="${cfg.package}/wiki/users.csv";
+          credentials="${cfg.dataDir}/users.csv";
           readers="(authenticated)";
           writers="(authenticated)";
         };
@@ -75,33 +103,39 @@
       };
     };
 
-		config = mkIf cfg.enable {
-      systemd = {
-        tmpfiles.rules = [
-          "d ${cfg.dataDir} 0755 root root - -"
-          "L ${cfg.dataDir}/tiddlywiki.info - - - - ${cfg.package}/wiki/tiddlywiki.info"
-          "L ${cfg.dataDir}/users.csv - - - - ${cfg.package}/wiki/users.csv"
-        ];
+    config_systemd = {
+      tmpfiles.rules = [
+        "d ${cfg.dataDir} 0755 root root - -"
+        "L+ ${cfg.dataDir}/tiddlywiki.info - - - - ${cfg.package}/tiddlywiki.info"
+        "L+ ${cfg.dataDir}/users.csv - - - - ${cfg.package}/users.csv"
+      ];
 
-        services.${service-name} =
+      services.${service-name} =
         let
-          restic-for-service = pkgs.writeShellScript "restic-${service-name}" ''
-            source ${cfg.backupCloudCredentialsFile}
+          repo = cfg.restore.backend.url;
+          repoCredentialsFile = cfg.restore.backend.credentialsFile;
+          passwordFile = cfg.restore.passwordFile;
 
-            ${pkgs.restic}/bin/restic -r ${cfg.backupRepo} -p ${cfg.backupPasswordFile} "$@"
-          '';
-          restore-data = pkgs.writeShellScript "restore-data-for-${service-name}.sh" ''
+          load-data-if-new-deploy = pkgs.writeShellScript "load-data-if-new-deploy-of-${service-name}.sh" ''
             set -euo pipefail
 
             if [ ! -d "${cfg.dataDir}/tiddlers" ]; then
-              ${restic-for-service} restore latest --target /tmp/${service-name}-backup/
+              source ${repoCredentialsFile}
 
-              TIDDLERS_FULL_PATH="$(find /tmp/${service-name}-backup/ -type d -name tiddlers)"
+              # without the explicit `export`, restic doesn't "see" the variables
+              export AWS_ACCESS_KEY_ID AWS_SECRET_ACCESS_KEY
 
-              mv "$TIDDLERS_FULL_PATH" "${cfg.dataDir}/tiddlers"
+              RESTIC_REPOSITORY="${repo}" \
+              RESTIC_PASSWORD_FILE=${passwordFile} \
+              ${pkgs.restic}/bin/restic restore latest --target /tmp/${service-name}-backup/
+
+              TIDDLERS_PATH="$(find /tmp/${service-name}-backup/ -type d -name tiddlers)"
+
+              mv "$TIDDLERS_PATH" "${cfg.dataDir}/tiddlers"
             fi
           '';
-					tiddlywiki = "${pkgs.nodePackages.tiddlywiki}/lib/node_modules/.bin/tiddlywiki";
+
+          tiddlywiki = "${pkgs.nodePackages.tiddlywiki}/lib/node_modules/.bin/tiddlywiki";
           listenParams = concatStrings (mapAttrsToList (n: v: " '${n}=${toString v}' ") cfg.listenOptions);
         in
           {
@@ -112,53 +146,71 @@
               Type = "simple";
               Restart = "on-failure";
               User = "root";
-              ExecStartPre = restore-data;
+              ExecStartPre = load-data-if-new-deploy;
               ExecStart = "${tiddlywiki} ${cfg.dataDir} --listen port=${toString(cfg.port)} ${listenParams}";
             };
           };
-      };
+    };
 
-      services.nginx = {
-        enable = true;
+    config_services_nginx = {
+      enable = true;
 
-        virtualHosts = {
-          "${cfg.domainName}" = {
-            forceSSL = true;
-            enableACME = true;
+      virtualHosts = {
+        "${cfg.domainName}" = {
+          forceSSL = true;
+          enableACME = true;
 
-            locations = {
-              "/" = {
-                proxyPass = "http://127.0.0.1:" + toString(cfg.port) + "/";
-                extraConfig = ''
-                  proxy_set_header        Host             $host;
-                  proxy_set_header        X-Real-IP        $remote_addr;
-                  proxy_set_header        X-Forwarded-For  $proxy_add_x_forwarded_for;
-                '';
-              };
+          locations = {
+            "/" = {
+              proxyPass = "http://127.0.0.1:" + toString(cfg.port) + "/";
+              extraConfig = ''
+                proxy_set_header        Host             $host;
+                proxy_set_header        X-Real-IP        $remote_addr;
+                proxy_set_header        X-Forwarded-For  $proxy_add_x_forwarded_for;
+              '';
             };
           };
-        }
-      };
-
-      restic.backups = {
-        remotebackup = {
-          initialize = false;
-          paths = [
-            "${cfg.dataDir}/tiddlers"
-          ];
-
-          repository = cfg.backupRepo;
-          passwordFile = cfg.backupPasswordFile;
-          environmentFile = cfg.backupCloudCredentialsFile;
-
-          timerConfig = {
-            OnCalendar = "*:00";
-          };
-          pruneOpts = [
-            "--keep-hourly 24"
-            "--keep-daily 7"
-          ];
         };
       };
     };
-  }
+
+    config_services_restic_backups = {
+      ${service-name} = {
+        initialize = false;
+        paths = [
+          "${cfg.dataDir}/tiddlers"
+        ];
+
+        repository = cfg.backup.backend.url;
+        passwordFile = cfg.backup.passwordFile;
+        environmentFile = cfg.backup.backend.credentialsFile;
+
+        timerConfig = {
+          OnCalendar = "*:00";
+        };
+
+        pruneOpts = [
+          "--keep-hourly 24"
+          "--keep-daily 7"
+        ];
+      };
+    };
+
+  in
+    {
+
+      options = {
+        services = {
+          ${service-name} = options;
+        };
+      };
+
+      config = mkIf cfg.enable {
+        systemd = config_systemd;
+
+        services = {
+          nginx = config_services_nginx;
+          restic.backups = config_services_restic_backups;
+        };
+      };
+    }

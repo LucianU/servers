204798a51bcbddc5bd732064d058c110acd68b88
Lucian Ursu <lucian.ursu@gmail.com>
2023-05-24 16:11:58 +0300

    Deploy new modules to staging

--- machines/do-nixos-stage/configuration.nix
+++ machines/do-nixos-stage/configuration.nix
@@ -2,6 +2,10 @@
 {
   imports = [
     ../../modules/tw-knowledge-store-v2.nix
+    ../../modules/tw-haskell-v2.nix
+    ../../modules/tw-rust-v2.nix
+    ../../modules/tw-sim.nix
+    ../../modules/tw-publish.nix
   ];
 
   boot = {
@@ -47,7 +51,10 @@
     age.sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
     secrets = {
       tw_stage_restic_pass = {};
-      tw_knowledge_store_v2_restic_pass = {};
+      tw_knowledge_store_restic_pass = {};
+      tw_haskell_restic_pass = {};
+      tw_rust_restic_pass = {};
+      tw_sim_restic_pass = {};
       digitalocean_spaces_credentials = {};
     };
   };
@@ -74,7 +81,95 @@
           url = "s3:fra1.digitaloceanspaces.com/know-elbear-com";
           credentialsFile = config.sops.secrets.digitalocean_spaces_credentials.path;
         };
-        passwordFile = config.sops.secrets.tw_knowledge_store_v2_restic_pass.path;
+        passwordFile = config.sops.secrets.tw_knowledge_store_restic_pass.path;
+      };
+    };
+
+    tw-haskell-v2 = {
+      enable = true;
+      port = 8081;
+      domainName = "haskell.staging.elbear.com";
+
+      backup = {
+        backend = {
+          url = "s3:fra1.digitaloceanspaces.com/stage-elbear-com";
+          credentialsFile = config.sops.secrets.digitalocean_spaces_credentials.path;
+        };
+        passwordFile = config.sops.secrets.tw_stage_restic_pass.path;
+      };
+
+      restore = {
+        backend = {
+          url = "s3:fra1.digitaloceanspaces.com/haskell-elbear-com";
+          credentialsFile = config.sops.secrets.digitalocean_spaces_credentials.path;
+        };
+        passwordFile = config.sops.secrets.tw_haskell_restic_pass.path;
+      };
+    };
+
+    tw-rust-v2 = {
+      enable = true;
+      port = 8082;
+      domainName = "rust.staging.elbear.com";
+
+      backup = {
+        backend = {
+          url = "s3:fra1.digitaloceanspaces.com/stage-elbear-com";
+          credentialsFile = config.sops.secrets.digitalocean_spaces_credentials.path;
+        };
+        passwordFile = config.sops.secrets.tw_stage_restic_pass.path;
+      };
+
+      restore = {
+        backend = {
+          url = "s3:fra1.digitaloceanspaces.com/rust-elbear-com";
+          credentialsFile = config.sops.secrets.digitalocean_spaces_credentials.path;
+        };
+        passwordFile = config.sops.secrets.tw_rust_restic_pass.path;
+      };
+    };
+
+    tw-sim = {
+      enable = true;
+      port = 8083;
+      domainName = "sim.staging.elbear.com";
+
+      backup = {
+        backend = {
+          url = "s3:fra1.digitaloceanspaces.com/stage-elbear-com";
+          credentialsFile = config.sops.secrets.digitalocean_spaces_credentials.path;
+        };
+        passwordFile = config.sops.secrets.tw_stage_restic_pass.path;
+      };
+
+      restore = {
+        backend = {
+          url = "s3:fra1.digitaloceanspaces.com/sim-elbear-com";
+          credentialsFile = config.sops.secrets.digitalocean_spaces_credentials.path;
+        };
+        passwordFile = config.sops.secrets.tw_sim_restic_pass.path;
+      };
+    };
+
+    tw-publish = {
+      enable = true;
+      port = 8084;
+      domainName = "publish.staging.elbear.com";
+
+      backup = {
+        backend = {
+          url = "s3:fra1.digitaloceanspaces.com/stage-elbear-com";
+          credentialsFile = config.sops.secrets.digitalocean_spaces_credentials.path;
+        };
+        passwordFile = config.sops.secrets.tw_stage_restic_pass.path;
+      };
+
+      restore = {
+        backend = {
+          url = "s3:fra1.digitaloceanspaces.com/stage-elbear-com";
+          credentialsFile = config.sops.secrets.digitalocean_spaces_credentials.path;
+        };
+        passwordFile = config.sops.secrets.tw_stage_restic_pass.path;
       };
     };
   };

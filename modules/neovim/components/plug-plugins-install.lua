64491bf7c7e5f31d930c50e958856b573dacdef2
Lucian Ursu <lucian.ursu@gmail.com>
2024-09-01 17:01:16 +0300

    Some cleanup

--- modules/neovim/components/plug-plugins-install.lua
+++ modules/neovim/components/plug-plugins-install.lua
@@ -6,15 +6,14 @@ vim.call('plug#begin', '~/.vim/plugged')
 
 Plug 'maxmx03/solarized.nvim'            -- Color Scheme
 Plug 'itchyny/lightline.vim'             -- Status line
+
 Plug 'junegunn/fzf'                      -- Fuzzy finder
-Plug 'junegunn/fzf.vim'                 -- FZF integration
-Plug 'gfanto/fzf-lsp.nvim'              -- Search for Symbols
+Plug 'junegunn/fzf.vim'                  -- FZF integration
+Plug 'gfanto/fzf-lsp.nvim'               -- Search for Symbols
 
 -- File browser
+Plug 'nvim-tree/nvim-web-devicons'
 Plug 'nvim-tree/nvim-tree.lua'
--- TODO: Icons are not displayed, even though I installed this plugin
--- There may be something wrong with my font. Need to investigate
--- Plug 'nvim-tree/nvim-web-devicons'
 
 -- Buffers
 Plug 'akinsho/bufferline.nvim'

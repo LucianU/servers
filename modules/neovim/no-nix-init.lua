4b5a29120473d5147d53c25f834a133fba3fc4f1
Lucian Ursu <lucian.ursu@gmail.com>
2024-09-24 15:33:35 +0300

    Fix loading of project-specific config

--- modules/neovim/no-nix-init.lua
+++ modules/neovim/no-nix-init.lua
@@ -48,6 +48,6 @@ require('lspconfig').zls.setup{
 
 -- Project-specific Config
 local project_config = vim.fn.getcwd() .. "/.nvim.lua"
-if vim.fn.filereadable(project_config) then
+if vim.fn.filereadable(project_config) == 1 then
   dofile(project_config)
 end
